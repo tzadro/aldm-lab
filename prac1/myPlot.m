841e3c18c1b18cabe0e0564d604514da432bb845
tzadro <tzadro94@gmail.com>
2018-05-07 21:46:51 +0200

    prac1 init

---
+++ prac1/myPlot.m
@@ -0,0 +1,8 @@
+fprintf('Plotting Data ...\n')
+data = load('ex1data1.txt');
+X = data(:, 1); y = data(:, 2);
+m = length(y); % number of training examples
+
+figure; % open a new figure window
+
+%write your plot code here
